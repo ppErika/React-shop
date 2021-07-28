6d630e460fca419d921140c8a6e11b72b5b40c81
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-28 15:20:58 +0900

    메인페이지와 디테일페이지

--- src/App.js
+++ src/App.js
@@ -23,32 +23,40 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="Jumbotron">
-        <div>
-          <h1>BIG SALE!!</h1>
-          <p>어쩌구 저쩌구 이런 저런 문구들</p>
-          <Button variant="primary">more</Button>
-        </div>
-      </div>
-
       <Route exact path="/">
-        <div>메인 페이지에요.</div>
-      </Route>
-      <Route path="/detail">
-        <div>상세 페이지에요.</div>
-      </Route>
-      {/* <Router path="/어쩌구" component={ Modal }></Router> */}
-
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((a, i) => {
-              return <Card shoes={shoes[i]} i={i} key={i} />
-            })
-          }
+        <div className="Jumbotron">
+          <div>
+            <h1>BIG SALE!!</h1>
+            <p>어쩌구 저쩌구 이런 저런 문구들</p>
+            <Button variant="primary">more</Button>
+          </div>
         </div>
-      </div>
+        <div className="container">
+          <div className="row">
+            {
+              shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i} key={i} />
+              })
+            }
+          </div>
+        </div>
+      </Route>
 
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
 
     </div>
   );
