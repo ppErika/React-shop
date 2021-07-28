53ac17370a6757e7efcfdd314838ea61c202ece2
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-28 12:32:46 +0900

    레이아웃 구성하기 (Navbar, Jumbotron, 상품(3분할))

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import logo from './logo.svg';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-outline-info">Info</button>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">shopSHOP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="Jumbotron">
+        <div>
+          <h1>BIG SALE!!</h1>
+          <p>어쩌구 저쩌구 이런 저런 문구들</p>
+          <Button variant="primary">이동하기</Button>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
