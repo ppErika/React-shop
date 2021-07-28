58175af415cdfb1123ff517e9b96744a72e8d34f
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-28 15:45:32 +0900

    함수도 모듈화할 수 있다 (Detail.js) / history를 사용하면 이전페이지나 원하는 페이지로 이동하도록 주소를 바꿔줄 수 있다 / switch는 한번에 하나의 Route만 보여줄 수 있게끔 해준다(exact 역할)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import './App.css';
 import ShoesData from './data.js';
-
+import Detail from './Detail.js';
 import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,49 +14,43 @@ function App() {
     <div className="App">
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">shopSHOP</Navbar.Brand>
+          <Navbar.Brand href="/">shopSHOP</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+            <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <Route exact path="/">
-        <div className="Jumbotron">
-          <div>
-            <h1>BIG SALE!!</h1>
-            <p>어쩌구 저쩌구 이런 저런 문구들</p>
-            <Button variant="primary">more</Button>
+      <Switch>
+        <Route exact path="/">
+          <div className="Jumbotron">
+            <div>
+              <h1>BIG SALE!!</h1>
+              <p>어쩌구 저쩌구 이런 저런 문구들</p>
+              <Button variant="primary">more</Button>
+            </div>
           </div>
-        </div>
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((a, i) => {
-                return <Card shoes={shoes[i]} i={i} key={i} />
-              })
-            }
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i} key={i} />
+                })
+              }
+            </div>
           </div>
-        </div>
-      </Route>
+        </Route>
 
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
-            </div>
-          </div>
-        </div>
-      </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을 때 이걸 보여줘</div>
+        </Route>
+        
+      </Switch>
 
     </div>
   );
