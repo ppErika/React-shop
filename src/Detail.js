3e14b0bc52de35c515adc20e670c19fae100100c
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-29 14:29:47 +0900

    sass 설치와 사용 (css내에서 함수는 @mixin -> @include를 통해 사용, 함수가 더 위에 있어야 함)

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import './Detail.scss'
 
 let 박스 = styled.div`
     padding: 20px;
 `;
-
 let 제목 = styled.h4`
     font-size: 25px;
-    color: ${ props => props.색상 }
+    color: ${ props => props.색상 };
 `;
 
 function Detail(props) {
@@ -22,9 +22,11 @@ function Detail(props) {
     return (
         <div className="container">
             <박스>
-                <제목 색상="blue">Detail</제목>
-                <제목 색상={'red'}>Detail</제목>
+                <제목 className="red">Detail</제목>
             </박스>
+            <div className="my-alert-yellow">
+                <p>재고가 얼마 남지 않았습니다!</p>
+            </div>
             <div className="row">
                 <div className="col-md-6">
                     <img src={'https://codingapple1.github.io/shop/shoes'+ (찾은상품.id+1) +'.jpg'} width="100%" />
